47f7a82220e520bb1608da030a3aa63c6975a02b
Zhihui Du <zhihuidu@gmail.com>
2021-03-19 14:16:27 -0400

    update string.py to easy merge

--- arkouda/strings.py
+++ arkouda/strings.py
@@ -2,14 +2,18 @@ from __future__ import annotations
 from typing import cast, Tuple, Union
 from typeguard import typechecked
 from arkouda.client import generic_msg
-from arkouda.pdarrayclass import pdarray, create_pdarray, parse_single_value,_parse_single_int_array_value
+from arkouda.pdarrayclass import pdarray, create_pdarray, parse_single_value
 from arkouda.logger import getArkoudaLogger
 import numpy as np # type: ignore
-from arkouda.dtypes import str as akstr
-from arkouda.dtypes import int64 as akint
+from arkouda.dtypes import npstr, int_scalars, str_scalars
 from arkouda.dtypes import NUMBER_FORMAT_STRINGS, resolve_scalar_dtype, \
      translate_np_dtype
 import json
+
+from arkouda.pdarrayclass import _parse_single_int_array_value
+from arkouda.dtypes import str as akstr
+from arkouda.dtypes import int64 as akint
+
 
 __all__ = ['Strings','SArrays']
 
